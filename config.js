cd0c0709687f223679a1b8e6d5455427b0736c11
guillaume.ouvrard@cap-technologie.com <guigui&85&>
2019-12-15 17:53:06 +0100

    Initial

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
-let dbfile = "/home/ouvrardg/WebstormProjects/domoticzdb2csv/domoticz.db";
-let csvFileName = "domoticzDbExport.csv";
-let listIdx = [125, 126, 127];
-
+// let dbfile = "/home/ouvrardg/WebstormProjectsdomoticzdb2csv/domoticz.db";
+let dbfile = "/home/pi/domoticz/domoticz.db";
+let csvMeterFileName = "domoticzMeterExport.csv";
+let csvTempFileName = "domoticzTempExport.csv";
+let listIdxMeter = [125, 126, 127];
+let listIdxTemp = [184];
 
 module.exports = {
     dbfile,
-    csvFileName,
-    listIdx
+    csvMeterFileName,
+    csvTempFileName,
+    listIdxMeter,
+    listIdxTemp
 };
