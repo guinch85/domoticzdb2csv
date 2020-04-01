f803283416b8a2fc271c0857cccca93b524322e5
guillaume.ouvrard@cap-technologie.com <guigui&85&>
2020-04-01 09:57:47 +0200

    Update list temp

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ let dbfile = "/home/pi/domoticz/domoticz.db";
 let csvMeterFileName = "domoticzMeterExport.csv";
 let csvTempFileName = "domoticzTempExport.csv";
 let listIdxMeter = [125, 126, 127];
-let listIdxTemp = [184, 458, 460, 464, 484, 492, 499, 546];
+let listIdxTemp = [458, 460, 484, 499, 546, 547, 553, 554];
 
 module.exports = {
     dbfile,
