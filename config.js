4dd747f8434e1443d9c014c10c4e0f06414da17d
Guillaume Ouvrard <guillaume.ouvrard@cap-technologie.com>
2020-04-29 19:58:59 +0200

    Update Garage Idx

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let csvMeterFileName = "domoticzMeterExport.csv";
 let csvTempFileName = "domoticzTempExport.csv";
 let csvHygroFileName = "domoticzHygroExport.csv";
 let listIdxMeter = [125, 126, 127];
-let listIdxTemp = [458, 460, 484, 555, 546, 547, 553, 554];
+let listIdxTemp = [458, 460, 484, 556, 546, 547, 553, 554];
 
 module.exports = {
     dbfile,
