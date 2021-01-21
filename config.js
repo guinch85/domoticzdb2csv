b5ef69319716151fa6bd27577a0a8eeacba71e5b
guillaume.ouvrard@cap-technologie.com <guigui&85&>
2021-01-21 21:02:49 +0100

    Update idx SalleDeBain

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ let csvTempFileName = "domoticzTempExport.csv";
 let csvHygroFileName = "domoticzHygroExport.csv";
 let listIdxMeter = [125, 126, 127];
 
-let listIdxTemp = [458, 460, 484, 546, 547, 553, 556, 924];
+let listIdxTemp = [458, 925, 484, 546, 547, 553, 556, 924];
 // Extérieure, Salle de bain, Chambre 1, Chambre 2, Salon, Cuisine, Garage, Bureau
 
 module.exports = {
