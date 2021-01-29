36a6fef05b09dbcc55733988dc23aa6df372bb09
guillaume.ouvrard@cap-technologie.com <guigui&85&>
2021-01-29 10:42:41 +0100

    Add power to export

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ let dbfile = "/home/pi/domoticz/domoticz.db";
 let csvMeterFileName = "domoticzMeterExport.csv";
 let csvTempFileName = "domoticzTempExport.csv";
 let csvHygroFileName = "domoticzHygroExport.csv";
-let listIdxMeter = [125, 126, 127];
+
+let listIdxMeter = [125, 126, 127, 128];
+//Amperage, HC, HP, Power
 
 let listIdxTemp = [458, 925, 484, 546, 547, 553, 556, 924];
 // Extérieure, Salle de bain, Chambre 1, Chambre 2, Salon, Cuisine, Garage, Bureau
