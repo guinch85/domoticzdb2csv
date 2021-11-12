34fee2abab8f457af04a7b4a033d08b93e7a2016
ouvrardg <guinch85@gmail.com>
2021-11-12 21:40:15 +0100

    add idx temp/hygro extension

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ let csvHygroFileName = "domoticzHygroExport.csv";
 let listIdxMeter = [125, 126, 127, 128, 1543, 1544, 1545];
 //Amperage, HC, HP, Power
 
-let listIdxTemp = [458, 484, 546, 1548, 943, 944, 951, 952, 953, 954, 1546, 1547];
+let listIdxTemp = [458, 484, 546, 1548, 943, 944, 951, 952, 953, 954, 1546, 1547, 1036, 1037];
 // Extérieure, Chambre 1, Chambre 2, Garage,
 
 module.exports = {
