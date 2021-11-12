740b838f13ec5349c1b03babd86d3bf423564168
ouvrardg <guinch85@gmail.com>
2021-11-12 21:15:57 +0100

    Update idx

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-//let dbfile = "/home/ouvrardg/WebstormProjects/domoticzdb2csv/test/domoticz.db";
+//let dbfile = "/home/ouvrardg/WebstormProjects/domoticzdb2csv/domoticz.db";
 let dbfile = "/home/pi/domoticz/domoticz.db";
 let csvMeterFileName = "domoticzMeterExport.csv";
 let csvTempFileName = "domoticzTempExport.csv";
@@ -7,8 +7,8 @@ let csvHygroFileName = "domoticzHygroExport.csv";
 let listIdxMeter = [125, 126, 127, 128, 1543, 1544, 1545];
 //Amperage, HC, HP, Power
 
-let listIdxTemp = [458, 925, 484, 546, 547, 553, 556, 924];
-// Extérieure, Salle de bain, Chambre 1, Chambre 2, Salon, Cuisine, Garage, Bureau
+let listIdxTemp = [458, 484, 546, 1548, 943, 944, 951, 952, 953, 954, 1546, 1547];
+// Extérieure, Chambre 1, Chambre 2, Garage,
 
 module.exports = {
     dbfile,
